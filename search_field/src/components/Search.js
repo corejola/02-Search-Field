03edebe41c9c1dd9bb6dd6d206a85dce716ee37e
Cedric Orejola <cedricorejola@gmail.com>
2020-10-26 00:20:18 -0700

    updated search.js routes

--- search_field/src/components/Search.js
+++ search_field/src/components/Search.js
@@ -18,7 +18,7 @@ class Search extends Component {
 
     // populate page with all data rows...
     componentDidMount() {
-        const url = `${process.env.PORT}/findAll`
+        const url = `/findAll`
         axios.get(url).then(res => {
             console.log(res.data)
             this.setState({
@@ -58,7 +58,7 @@ class Search extends Component {
 
         console.log(query)
 
-        const searchUrl = `${process.env.PORT}/search=${query}`
+        const searchUrl = `/search=${query}`
 
         if (this.cancel) {
             this.cancel.cancel()
