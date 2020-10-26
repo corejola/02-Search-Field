3d587191c90268448aedc854cf50be665a935fea
Cedric Orejola <cedricorejola@gmail.com>
2020-10-26 00:00:06 -0700

    revisions for heroku

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(express.static(path.join(__dirname, "search_field", 'public')))
+app.use(express.static(path.join(__dirname, "search_field", 'build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "search_field", 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, "search_field", 'build', 'index.html'));
 });
 // ----Deployment----
 
