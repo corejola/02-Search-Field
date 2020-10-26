e171793114be3549a0c7fbb93642c85a701310b9
Cedric Orejola <cedricorejola@gmail.com>
2020-10-26 00:11:19 -0700

    try again

--- search_field/src/components/Search.js
+++ search_field/src/components/Search.js
@@ -18,7 +18,7 @@ class Search extends Component {
 
     // populate page with all data rows...
     componentDidMount() {
-        const url = `/findAll`
+        const url = `${process.env.PORT}/findAll`
         axios.get(url).then(res => {
             console.log(res.data)
             this.setState({
@@ -58,7 +58,7 @@ class Search extends Component {
 
         console.log(query)
 
-        const searchUrl = `/search=${query}`
+        const searchUrl = `${process.env.PORT}/search=${query}`
 
         if (this.cancel) {
             this.cancel.cancel()
