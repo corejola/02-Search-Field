401ba67b891efbd744eeb00ac68340f4e07760e6
Cedric Orejola <cedricorejola@gmail.com>
2020-10-26 10:19:37 -0700

    rebuild

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 
 // For Deployment
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 app.use(express.static(path.join(__dirname, "search_field", "build")))
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, "search_field", "build", "index.html"));
 });
 // ----Deployment----
