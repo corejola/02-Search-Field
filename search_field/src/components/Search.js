ef838f532a0b9b473a761e45704eb526aee0c29a
Cedric Orejola <cedricorejola@gmail.com>
2020-10-26 09:32:12 -0700

    rebuild

--- search_field/src/components/Search.js
+++ search_field/src/components/Search.js
@@ -3,12 +3,12 @@ import './Search.css';
 import axios from 'axios';
 import Loader from './gif/loader.gif';
 
-// API Server variable
-// let serverAPI = "";
+// URL Server variable
+// let serverURL = "";
 // if (process.env.NODE_ENV === "production") {
-//     serverAPI = process.env.PORT;
+//     serverURL = process.env.PORT;
 // } else {
-//     serverAPI = "http://localhost:8000";
+//     serverURL = "http://localhost:8000";
 // }
 
 // using class component
@@ -27,7 +27,6 @@ class Search extends Component {
     // populate page with all data rows...
     componentDidMount() {
         const url = '/findAll'
-        console.log(url)
         axios.get(url).then(res => {
             this.setState({
                 results: res.data
@@ -35,6 +34,8 @@ class Search extends Component {
         }).catch(err => {
             console.log(err)
         })
+
+        console.log(this.state.results[0])
 
     }
 
